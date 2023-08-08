dacefa2cc79795e1383e80cc3344a43f83f86899
zhangwei-kumo <kanseefoil@gmail.com>
2023-08-08 17:05:32 +0800

    debug

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -688,16 +688,18 @@ export function Chat() {
     }
     // 将当前用户的输入值暂存
     let tem = userInput;
-    // 清除输入框
+    setUserInput("");
+
+    localStorage.setItem(LAST_INPUT_KEY, tem);
     setIsLoading(true);
+
     // 向私有数据库发起请求
     const res = await fetch("/api/private/" + tem);
-    // let { context } = await res.json();
-    chatStore.onUserInput(tem, "context").then(() => setIsLoading(false));
+    let { context } = await res.json();
+    chatStore.onUserInput(tem, context).then(() => setIsLoading(false));
     // 存储最近的输入
-    localStorage.setItem(LAST_INPUT_KEY, tem);
     console.log("本地向量数据库返回结果：", context);
-    setUserInput("");
+
     setPromptHints([]);
     if (!isMobileScreen) inputRef.current?.focus();
     setAutoScroll(true);
